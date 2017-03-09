5619a7fb68f1471e3d494df258e2b6c78b3860f6
Bao Vuong <vqbao@lv5.org>
2017-03-08 19:48:34 -0500

    actual unit test

--- test/test_main.c
+++ test/test_main.c
@@ -2,32 +2,36 @@
 #include <CUnit/Basic.h>
 #include <stdio.h>
 
-void simpleTest(void) {
-        CU_ASSERT(2 == 2);
+#include "../src/music_note.h"
+
+// music note
+void midi_notes_test(void) {
+    music_note_t concert_a = {A, 4};
+    CU_ASSERT(57 == music_note_to_midi_value(concert_a));
 }
- 
+
 int main (int argc, char** argv) {
- 
+
         CU_pSuite pSuite = NULL;
- 
-        /* initialize the CUnit test registry */ 
+
+        /* initialize the CUnit test registry */
    if (CUE_SUCCESS != CU_initialize_registry())
       return CU_get_error();
- 
-   /* add a suite to the registry */ 
+
+   /* add a suite to the registry */
    pSuite = CU_add_suite("Suite_1", NULL, NULL);
    if (NULL == pSuite) {
       CU_cleanup_registry();
       return CU_get_error();
    }
- 
-   /* add the tests to the suite */ 
-   if (NULL == CU_add_test(pSuite, "Simple Addition Test", simpleTest)) {
+
+   /* add the tests to the suite */
+   if (NULL == CU_add_test(pSuite, "Midi Notes", midi_notes_test)) {
       CU_cleanup_registry();
       return CU_get_error();
    }
- 
-   /* Run all tests using the CUnit Basic interface */ 
+
+   /* Run all tests using the CUnit Basic interface */
    CU_basic_set_mode(CU_BRM_VERBOSE);
    CU_basic_run_tests();
    CU_cleanup_registry();
